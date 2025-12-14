38c0d381184eec2e0087a13ff702f08f85a413af
Manjunatha B <bmanjunath@users.noreply.github.com>
2025-12-14 22:18:19 +0530

    Update playwright.config.ts

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,14 +1,16 @@
-import { defineConfig, devices } from '@playwright/test';
+import { defineConfig } from '@playwright/test';
+import * as dotenv from 'dotenv';
+dotenv.config();
 
 export default defineConfig({
   testDir: './tests',
   timeout: 30000,
   retries: 1,
   use: {
-    browserName: 'chromium',
-    headless: false,
+    browserName: process.env.BROWSER || 'chromium',
+    headless: process.env.HEADLESS === 'true' ? true : false,
     screenshot: 'only-on-failure',
     video: 'retain-on-failure',
-    baseURL: 'https://www.flipkart.com',
+    baseURL: process.env.BASE_URL || 'https://www.flipkart.com',
   },
 });
